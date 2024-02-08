c94d0050af712232cc71b8fdc039ee8743861876
EmilseBilse <71692416+EmilseBilse@users.noreply.github.com>
2024-02-08 12:30:07 +0100

    login

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,32 @@
 <script>
 
 </script>
-<nav>
-    <a href="/">home</a>
-    <a href="/get">get</a>
-    <a href="/create">create</a>
-</nav>
+<article class="centering colomns">
+    <nav class="centering">
+        <a href="/" class="navBarTextStyling">home</a>
+        <a href="/get" class="navBarTextStyling">get</a>
+        <a href="/create" class="navBarTextStyling">create</a>
+        <a href="/login" class="navBarTextStyling">login</a>
+    </nav>
 
-<slot></slot>
+    <slot></slot>
+</article>
+
 
 <style>
+    .centering {
+        display: flex;
+        justify-content: center;
+        gap: 2rem;
+    }
+
+    .navBarTextStyling {
+        color: rgb(49, 49, 61);
+        font-size: larger;
+        text-decoration: none;
+    }
+    .colomns {
+        flex-direction: column;
+    }
     
 </style>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+
+</script>
+
+<article class="centering">
+
+    <form class="gapping">
+        <div class="lblInput">
+            <label for="username">Username:</label>
+            <input id="username" type="text" placeholder="Example"/>
+        </div>
+        <div class="lblInput">
+            <label for="password">Password:</label>
+            <input id="password" type="password" placeholder="****"/>
+        </div>
+
+        <button>Login</button>
+
+        <a class="link" href="/signup">
+            Dont have an account?
+            <div class="signup">signup!</div>
+        </a>
+    </form>
+</article>
+
+<style>
+    .link {
+        color: black;
+        text-decoration: none;
+        display: flex;
+        gap: 0.25rem;
+        justify-content: center;
+    }
+    
+    .signup {
+        color: blue;
+        text-decoration: underline;
+    }
+
+    .centering {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .gapping {
+        display: flex;
+        gap: 1rem;
+        flex-direction: column;
+    }
+
+    .lblInput {
+        display: flex;
+        justify-content: space-between;
+        gap: 0.5rem;
+    }
+    
+</style>
