7a66a609aa5462aaca9ee7c9db67392ed0c6297a
EmilseBilse <bandittenxd@gmail.com>
2024-02-09 13:46:16 +0100

    token and login

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,32 +1,53 @@
-<script>
+<script lang="ts">
+	import { tokenStore } from '../stores/userStore'; // Adjust the path as necessary
 
+	function logout() {
+		localStorage.removeItem('token');
+		tokenStore.set(null); // Update the store instead of a local variable
+	}
 </script>
-<article class="centering colomns">
-    <nav class="centering">
-        <a href="/" class="navBarTextStyling">home</a>
-        <a href="/get" class="navBarTextStyling">get</a>
-        <a href="/create" class="navBarTextStyling">create</a>
-        <a href="/login" class="navBarTextStyling">login</a>
-    </nav>
 
-    <slot></slot>
+<article class="centering columns">
+	<nav class="centering">
+		<a href="/" class="navBarTextStyling">home</a>
+		<a href="/get" class="navBarTextStyling">get</a>
+		<a href="/create" class="navBarTextStyling">create</a>
+		{#if !$tokenStore}
+			<a href="/login" class="navBarTextStyling">login</a>
+		{:else}
+			<button on:click={logout} class="removeBtnStyling">logout</button>
+		{/if}
+	</nav>
+
+	<slot />
 </article>
 
-
 <style>
-    .centering {
-        display: flex;
-        justify-content: center;
-        gap: 2rem;
-    }
+	.centering {
+		display: flex;
+		justify-content: center;
+		gap: 2rem;
+	}
 
-    .navBarTextStyling {
-        color: rgb(49, 49, 61);
-        font-size: larger;
-        text-decoration: none;
-    }
-    .colomns {
-        flex-direction: column;
-    }
-    
+	.navBarTextStyling {
+		color: rgb(49, 49, 61);
+		font-size: larger;
+		text-decoration: none;
+	}
+	.columns {
+		flex-direction: column;
+	}
+
+	.removeBtnStyling {
+		background: none;
+		color: inherit;
+		border: none;
+		padding: 0;
+		font: inherit;
+		cursor: pointer;
+		outline: inherit;
+		color: rgb(49, 49, 61);
+		font-size: larger;
+		text-decoration: none;
+	}
 </style>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,58 +1,96 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
+	import { tokenStore } from '../../stores/userStore';
 
+	let username: '';
+	let password: '';
+	const loginUrl = 'http://localhost:4000/api/user/login';
+
+	async function login() {
+		try {
+			const response = await fetch(loginUrl, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({
+					name: username,
+					password
+				})
+			});
+
+			if (!response.ok) {
+				throw new Error('Network response was not ok!');
+			}
+
+			const result = await response.json();
+
+			if (result.error) {
+				console.error('Login failed:', result.error);
+				return null;
+			}
+
+			const { token } = result.data;
+
+			localStorage.setItem('token', token);
+			tokenStore.set(token); // Set the token in the store
+			goto('/');
+		} catch (error) {
+			console.error('Error during login:', error);
+			return null;
+		}
+	}
 </script>
 
 <article class="centering">
+	<form class="gapping" on:submit|preventDefault={login}>
+		<div class="lblInput">
+			<label for="username">Username:</label>
+			<input id="username" type="text" bind:value={username} placeholder="Example" />
+		</div>
+		<div class="lblInput">
+			<label for="password">Password:</label>
+			<input id="password" type="password" bind:value={password} placeholder="****" />
+		</div>
 
-    <form class="gapping">
-        <div class="lblInput">
-            <label for="username">Username:</label>
-            <input id="username" type="text" placeholder="Example"/>
-        </div>
-        <div class="lblInput">
-            <label for="password">Password:</label>
-            <input id="password" type="password" placeholder="****"/>
-        </div>
+		<button>Login</button>
 
-        <button>Login</button>
-
-        <a class="link" href="/signup">
-            Dont have an account?
-            <div class="signup">signup!</div>
-        </a>
-    </form>
+		<a class="link" href="/signup">
+			Dont have an account?
+			<div class="signup">signup!</div>
+		</a>
+	</form>
 </article>
 
 <style>
-    .link {
-        color: black;
-        text-decoration: none;
-        display: flex;
-        gap: 0.25rem;
-        justify-content: center;
-    }
-    
-    .signup {
-        color: blue;
-        text-decoration: underline;
-    }
+	.link {
+		color: black;
+		text-decoration: none;
+		display: flex;
+		gap: 0.25rem;
+		justify-content: center;
+	}
 
-    .centering {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    }
+	.signup {
+		color: blue;
+		text-decoration: underline;
+	}
 
-    .gapping {
-        display: flex;
-        gap: 1rem;
-        flex-direction: column;
-    }
+	.centering {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
 
-    .lblInput {
-        display: flex;
-        justify-content: space-between;
-        gap: 0.5rem;
-    }
-    
+	.gapping {
+		display: flex;
+		gap: 1rem;
+		flex-direction: column;
+	}
+
+	.lblInput {
+		display: flex;
+		justify-content: space-between;
+		gap: 0.5rem;
+	}
 </style>
