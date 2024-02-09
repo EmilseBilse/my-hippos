f43390393acdbb91978f9d80bcf68c49b3e4f544
EmilseBilse <bandittenxd@gmail.com>
2024-02-09 17:18:41 +0100

    hippo card

---
+++ src/components/hippo-card.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+</script>
+
+<article>
+	<div class="card">
+		<div class="card-header">
+			<h2>Hippo Details</h2>
+		</div>
+		<div class="card-body">
+			<p><strong>Name:</strong> Hippo Name</p>
+			<p><strong>Weight (kg):</strong> 1500</p>
+			<p><strong>Color:</strong> Gray</p>
+			<p><strong>Habitat:</strong> Rivers and Lakes</p>
+			<p><strong>Max Speed (km/hr):</strong> 30</p>
+			<p><strong>Birth Date:</strong> 2010-04-25</p>
+		</div>
+	</div>
+</article>
+
+<style>
+	.card {
+		background-color: #cacaca;
+		border: 1px solid #ddd;
+		border-radius: 8px;
+		box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+		max-width: 400px;
+		margin: 20px auto;
+		padding: 20px;
+	}
+
+	.card-header h2 {
+		margin: 0;
+		padding-bottom: 10px;
+		border-bottom: 1px solid #eee;
+		color: #333;
+	}
+
+	.card-body p {
+		color: #666;
+		line-height: 1.6;
+		margin: 10px 0;
+	}
+
+	.card-body p strong {
+		color: #333;
+	}
+</style>
--- src/routes/get/+page.svelte
+++ src/routes/get/+page.svelte
@@ -1,1 +1,23 @@
-get
+<script lang="ts">
+	import HippoCard from '../../components/hippo-card.svelte';
+</script>
+
+<article class="container">
+	<HippoCard></HippoCard>
+	<HippoCard></HippoCard>
+	<HippoCard></HippoCard>
+	<HippoCard></HippoCard>
+	<HippoCard></HippoCard>
+	<HippoCard></HippoCard>
+	<HippoCard></HippoCard>
+</article>
+
+<style>
+	.container {
+		display: grid;
+		grid-gap: 20px;
+		justify-content: center;
+		padding: 20px;
+		grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+	}
+</style>
